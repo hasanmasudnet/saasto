10c9ad54920e6b6a077c1e25a77a9ea56b68527c
Md Kaium Hossain Molla <rian.nits90@gmail.com>
2023-04-30 17:57:46 +0600

    aa

--- page-templates/contact-two.php
+++ page-templates/contact-two.php
@@ -21,63 +21,66 @@
 
 <div class="contact-one-area section-gap-xl-top section-gap-xl-bottom">
    <div class="container">
-      <div class="row justify-content-between align-items-center">
-         <div class="col-lg-5">
-            <div class="w-feature-content-title">
-               <h3 class="text-bright-gray">Let’s get in touch!</h3>
-               <p class="text-purple-haze body-disply-1 pt-3">Our co-founders Tuomo Riekki and Kristo Ovaska are serial entrepreneurs who, back in 2013, noticed how advanced marketer</p>
+      <div class="row">
+         <div class="col-lg-12">
+            <div class="w-feature-content-title text-center">
+               <h3 class="text-bright-gray">Contact Us</h3>
+               <p class="text-purple-haze body-disply-1 pt-3">Our co-founders Tuomo Riekki and Kristo Ovaska are serial entrepreneurs who</p>
             </div>
-            <div class="contact-address-wrap mt-5">
-               <div class="single-contact-address d-flex align-items-center">
-                  <div class="contact-address-icon">
-                     <i class="bi bi-geo-alt"></i>
-                  </div>
-                  <div class="contact-address-info">
-                     <h2>Head Office</h2>
-                     <p>123 Maint St Anytown, USA</p>
-                  </div>
-               </div>
-               <div class="single-contact-address d-flex align-items-center">
-                  <div class="contact-address-icon">
-                     <i class="bi bi-telephone"></i>
-                  </div>
-                  <div class="contact-address-info">
-                     <h2>Call Us</h2>
-                     <p><a href="tel:928-456-1234">928-456-1234</a></p>
-                  </div>
-               </div>
-               <div class="single-contact-address d-flex align-items-center">
-                  <div class="contact-address-icon">
-                     <i class="bi bi-envelope"></i>
-                  </div>
-                  <div class="contact-address-info">
-                     <h2>E-Mail Us</h2>
-                     <p><a href="mailto:yourmail@gmail.com">yourmail@gmail.com</a></p>
-                  </div>
+         </div>
+      </div>
+      <div class="row mt-4 align-items-center gy-4">
+         <div class="col-lg-4 col-md-6">
+            <div class="contact-vector-box text-center">
+               <img src=<?php echo get_template_directory_uri(  )?>.'../assets/img/contact-vector.png' alt="">
+               <h2>E-mail Address</h2>
+               <a href="mailto:yourname@gmail.com">yourname@gmail.com</a>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="contact-vector-box text-center">
+               <img src=<?php echo get_template_directory_uri(  )?>.'../assets/img/contact-vector.png' alt="">
+               <h2>Support Number</h2>
+               <a href="tel:021000000">+021-000-000</a>
+               <div class="contact-vector-btn-wrap">
+                  <a class="contact-vector-btn" href="tel:021000000">Call Now <i class="bi bi-arrow-right"></i></a>
                </div>
             </div>
          </div>
-         <div class="col-lg-6">
-            <div class="contact-form-wrap">
+         <div class="col-lg-4 col-md-6">
+            <div class="contact-vector-box text-center">
+               <img src=<?php echo get_template_directory_uri(  )?>.'../assets/img/contact-vector.png' alt="">
+               <h2>Head Office</h2>
+               <a href="#">123 St Anytown, USA</a>
+            </div>
+         </div>
+      </div>
+      <div class="row section-gap-xl-top">
+         <div class="col-lg-12">
+            <div class="w-feature-content-title">
+               <h3 class="text-bright-gray">Let’s get in touch!</h3>
+               <p class="text-purple-haze body-disply-1 pt-3">Our co-founders Tuomo Riekki and Kristo Ovaska are advanced marketer</p>
+            </div>
+            <div class="contact-form-wrap-two  mt-4">
                <form action="#">
                   <div class="row">
-                     <div class="col-lg-12">
+                     <div class="col-lg-6">
                         <input type="text" placeholder="First name">
                      </div>
-                     <div class="col-lg-12">
+                     <div class="col-lg-6">
                         <input type="text" placeholder="Last name">
                      </div>
-                     <div class="col-lg-12">
+                     <div class="col-lg-6">
                         <input type="tel" placeholder="Phone">
                      </div>
-                     <div class="col-lg-12">
+                     <div class="col-lg-6">
                         <input type="email" placeholder="E-mail">
                      </div>
                      <div class="col-lg-12">
                         <textarea cols="30" rows="4" placeholder="Your Message"></textarea>
                      </div>
                      <div class="col-lg-12">
-                        <button class="border-0 w-100 d-inline-block" type="submit">Send Message</button>
+                        <button class="border-0 d-inline-block btn-fill-rounded" type="submit">Send Message</button>
                      </div>
                   </div>
                </form>
